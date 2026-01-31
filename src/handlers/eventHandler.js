b523939ac8a6266f3629f3a448624716b1032f92
Harel <harelikatz@gmail.com>
2026-01-31 14:55:43 +0200

    fixed events not registering

--- src/handlers/eventHandler.js
+++ src/handlers/eventHandler.js
@@ -2,17 +2,15 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = (client) => {
-  const eventsPath = path.join(__dirname, '..', 'events');
 
-  const files = fs.readdirSync(eventsPath);
+  const eventsPath = path.join(__dirname, '..', 'events');
+  const files = fs.readdirSync(eventsPath).filter(f => f.endsWith('.js'));
 
   for (const file of files) {
+
     const event = require(path.join(eventsPath, file));
 
-    if (event.once)
-      client.once(event.name, (...args) => event.execute(...args, client));
-    else
-      client.on(event.name, (...args) => event.execute(...args, client));
+    client.on(event.name, (...args) => event.execute(...args, client));
   }
 
   console.log('✅ Events loaded');
